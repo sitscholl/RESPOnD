581647d501fd27d42c3e7e1ef7162616b055187f
sitscholl <tscholl.simon@gmail.com>
2024-10-15 15:53:26 +0200

    Switched to dynamic GCM-RCM selection based on provided bias-adjustment method and included some tests to calculate day of year when phenological threshold is reached

--- 2_prepare_cordex.py
+++ 2_prepare_cordex.py
@@ -4,6 +4,7 @@ import pandas as pd
 import requests
 import urllib3
 import datetime
+import warnings
 
 import numpy as np
 import xarray as xr
@@ -27,95 +28,108 @@ if not lm.is_logged_on():
     raise ValueError('Log on failed!')
 
 # Execute Query
-# bbox = {'north': 14.864502, 'south': 8.360596, 'west': 45.790509, 'east': 47.561701}
 search_args = {
     'project': 'CORDEX-Adjust',
     'domain': 'EUR-11',
     'time_frequency': 'day',
     'from_timestamp': "1980-01-01T00:00:00Z",
     'to_timestamp': "2100-12-31T23:59:00Z",
-    'facets': 'driving_model,rcm_name,bias_adjustment,ensemble',
+    'ensemble': 'r1i1p1',
+    'experiment': 'rcp45',
+    "bias_adjustment": 'v1-LSCE-IPSL-CDFt-EOBS10-1971-2005',
+    'facets': 'driving_model,rcm_name,variable',
     'latest': True,
 }
 
-chains = {
-    'ICHEC-EC-EARTH': [('RACMO22E', 'r1i1p1'), ('HIRHAM5', 'r3i1p1'), ('CCLM4-8-17', 'r12i1p1'), ('RCA4', 'r12i1p1')],
-    'MOHC-HadGEM2-ES': [('CCLM4-8-17', 'r1i1p1'), ('RACMO22E', 'r1i1p1'), ('RCA4', 'r1i1p1')],
-    'MPI-M-MPI-ESM-LR': [('CCLM4-8-17', 'r1i1p1'), ('REMO2009', 'r1i1p1'), ('RCA4', 'r1i1p1')],
-    'IPSL-IPSL-CM5A-MR': [('RCA4', 'r1i1p1')],
-    'NCC-NorESM1-M': [('RCA4', 'r1i1p1')],
-    #'CNRM-CERFACS-CNRM-CM5': []
-}
 variables = ['tasAdjust', 'prAdjust']
 
+chains_overview = pd.DataFrame(columns = ['GCM', 'RCM', 'variable'])
+
 conn = SearchConnection('https://esgf-data.dkrz.de/esg-search', distrib=True)
-# ctx = conn.new_context(bias_adjustment = 'v1-LSCE-IPSL-CDFt-EOBS10-1971-2005', **search_args)
+ctx = conn.new_context(**search_args)
+gcms = list(ctx.facet_counts['driving_model'].keys())
+for gcm in gcms:
+    ctx_gcm = ctx.constrain(driving_model = gcm)
+    rcms = list(ctx_gcm.facet_counts['rcm_name'].keys())
 
-# ctx.facet_counts['driving_model']
-# ctx.facet_counts['bias_adjustment']
-# ctx.facet_counts['rcm_name']
-# ctx.facet_counts['ensemble']
+    for rcm in rcms:
+        ctx_rcm = ctx_gcm.constrain(rcm_name = rcm)
+        vars_list = list(ctx_rcm.facet_counts['variable'].keys())
 
-all_files = []
-for gcm, rcms in chains.items():
-    for rcm, ensemble in rcms:
-        for var in variables:
-
-            ctx = conn.new_context(
-                driving_model=gcm,
-                rcm_name=rcm,
-                experiment="rcp45",
-                variable=var,
-                ensemble=ensemble,
-                #bias_adjustment = 'v1-LSCE-IPSL-CDFt-EOBS10-1971-2005',
-                **search_args
-            )
-            ds_hist = ctx.search()
-
-            if len(ds_hist) > 1:
-                #raise ValueError('More than 1 dataset found. Refine search!')
-                print(f'More than 1 dataset found for {gcm}-{rcm}-{ensemble}-{var}')
-                print(ctx.facet_counts['bias_adjustment'])
-                continue
-            elif len(ds_hist) == 0:
-                print(f'No datasets found for {gcm}-{rcm}-{ensemble}-{var}')
-                continue
-
-            files = ds_hist[0].file_context().search()
-            fnames = [i.filename for i in files]
-            #urls = [i.opendap_url for i in files]
-
-            all_files.append(fnames)
-            print(f"{gcm}-{rcm}-{ensemble}-{var}")
+        if not all([i in vars_list for i in variables]):
+            warnings.warn(f"Not all variables present for {gcm}-{rcm}")
             continue
 
-            # xr.open_dataset(files[0].opendap_url)
+        ds = []
+        for var in variables:
+            ctx_var = ctx_rcm.constrain(variable = var)
+            dataset = ctx_var.search()
 
-            sys.exit()
+            if len(dataset) != 1:
+                warnings.warn(f'{len(dataset)} files found for {gcm}-{rcm}-{var}!')
+                continue
+
+            files = dataset[0].file_context().search()
+            fnames = [i.filename for i in files]
+            urls = [i.opendap_url for i in files]
+
+            sdates = [datetime.datetime.strptime(i.split('_')[-1].split('-')[0], '%Y%m%d') for i in fnames]
+            urls_sel = [url for i,url in enumerate(urls) if sdates[i] >= datetime.datetime(1980, 1, 1)]
+
+            ds_var = xr.open_mfdataset(urls_sel)[var]
+            ds_var = ds_var.sel(rlat = slice(-5, 0), rlon = slice(-10, -5))
+
+            ds.append(ds_var)
+
+            #chains_overview.loc[len(chains_overview)] = (gcm, rcm, var)
+
+        if len(set([i.shape for i in ds])) > 1:
+            raise ValueError(f'Shape mismatch between variables for {gcm}-{rcm}!')
+        ds = xr.merge(ds)
 
 
+chains_overview['bool'] = 1
+chains_overview.pivot(index = ['GCM', 'RCM'], columns='variable')
 
 
+Fcrit = 2286
 
-
-test = xr.open_mfdataset(urls[0:2], preprocess = lambda x: x.sel(rlat = slice(-5, 0), rlon = slice(-10, -5)))
-test.tasAdjust.isel(time = 0).plot()
-test.to_netcdf('test.nc')
-
-test = xr.open_dataset(r"C:\Users\tscho\Downloads\tas_EUR-11_ICHEC-EC-EARTH_historical_r12i1p1_CLMcom-CCLM4-8-17_v1_day_19491201-19501231.nc")
-test.sel(rlat = slice(-5, 0), rlon = slice(-10, -5)).tas.isel(time = 0).plot()
-
-test = results[1].file_context()#.search()
-
-test_df = pd.DataFrame(list(
-    map(
-        lambda f: {
-            "filename": f.filename,
-            "url": f.download_url,
-            "opendap": f.opendap_url,
-        },
-        test,
+##Calculate temperature cumsum for each year after 60th doy
+tas_sum = (
+    (
+        ds.tasAdjust.sel(time=(ds.time.dt.dayofyear >= 60))# & (ds.time.dt.year <= 1983))
+        - 273.5
     )
-))
-test_df['Period'] = test_df['filename'].str.split('_', expand = True).iloc[:,-1]
-test_df['url'][0]
+    .clip(min=0)
+    .groupby("time.year")
+    .cumsum()
+    .compute()
+)
+
+##Subtract Fcrit per variety and create mask
+tas_diff = tas_sum - Fcrit
+tas_diff = tas_diff.where(tas_diff >= 0)
+mask_diff = tas_diff.groupby('time.year').apply(lambda x: x.notnull().any('time')) #Finds pixels that reach Fcrit during the year
+
+##Get date when Fcrit is reached for each year
+Fcrit_date = tas_diff.groupby('time.year').apply(lambda c: c.idxmin(dim="time"))
+
+##Expand array to add 45 days after Fcrit is reached
+#test4 = test3.expand_dims(nr = 1).interp(nr = np.arange(45)+1)
+Fcrit_range = xr.concat(
+    [
+        (Fcrit_date + np.timedelta64(i, "D")).assign_coords({"nr": i})
+        for i in np.arange(45) + 1
+    ],
+    dim="nr",
+)
+
+##Remove dates that 'jumped to' next year
+Fcrit_range = Fcrit_range.where(Fcrit_range.dt.month > 3)
+plt.clf()
+Fcrit_range.isel(year = 0, nr = -1).dt.dayofyear.plot()
+
+##Get climatic variables within 45 day window for valid pixels
+clim_window = ds.sel(time = Fcrit_range, method = 'nearest').where(mask_diff) #method = 'nearest' needed, otherwise error due to NAN values
+plt.clf()
+clim_window.tasAdjust.isel(year = 0, nr = -1).plot()
