1dfa2947ff17fb8027a42cbb3a01db978120cc54
sitscholl <tscholl.simon@gmail.com>
2024-10-15 10:42:49 +0200

    First implementation with fixed chains and GCM-RCM chains based on Swiss Report

--- 2_prepare_cordex.py
+++ 2_prepare_cordex.py
@@ -15,6 +15,8 @@ from pyesgf.search import SearchConnection
 from pyesgf.logon import LogonManager
 # import xesmf as xe
 
+os.environ["ESGF_PYCLIENT_NO_FACETS_STAR_WARNING"] = "on"
+
 # Log into ESGF Portal
 openid = "https://esgf.nci.org.au/esgf-idp/openid/sitscholl"
 lm = LogonManager()
@@ -25,65 +27,76 @@ if not lm.is_logged_on():
     raise ValueError('Log on failed!')
 
 # Execute Query
-bbox = {'north': 14.864502, 'south': 8.360596, 'west': 45.790509, 'east': 47.561701}
-search_dict = {
+# bbox = {'north': 14.864502, 'south': 8.360596, 'west': 45.790509, 'east': 47.561701}
+search_args = {
     'project': 'CORDEX-Adjust',
     'domain': 'EUR-11',
     'time_frequency': 'day',
-    'variable': ['tasAdjust', 'prAdjust'],
     'from_timestamp': "1980-01-01T00:00:00Z",
     'to_timestamp': "2100-12-31T23:59:00Z",
-    'facets': 'driving_model,rcm_name,bias_adjustment',
+    'facets': 'driving_model,rcm_name,bias_adjustment,ensemble',
     'latest': True,
-    #'bbox': bbox
 }
 
-# Todo: Further differentiate RCMs by init id (r1i1p1 etc.)
 chains = {
-    'ICHEC-EC-EARTH': ['RACMO22E', 'HIRHAM5', 'CCLM4-8-17', 'RCA4'],
-    'MOHC-HadGEM2-ES': ['CCLM4-8-17', 'RACMO22E', 'RCA4'],
-    'MPI-M-MPI-ESM-LR': ['CCLM4-8-17', 'REMO2009', 'RCA4'],
-    'IPSL-IPSL-CM5A-MR': ['RCA4'],
-    'NCC-NorESM1-M': ['RCA4'],
+    'ICHEC-EC-EARTH': [('RACMO22E', 'r1i1p1'), ('HIRHAM5', 'r3i1p1'), ('CCLM4-8-17', 'r12i1p1'), ('RCA4', 'r12i1p1')],
+    'MOHC-HadGEM2-ES': [('CCLM4-8-17', 'r1i1p1'), ('RACMO22E', 'r1i1p1'), ('RCA4', 'r1i1p1')],
+    'MPI-M-MPI-ESM-LR': [('CCLM4-8-17', 'r1i1p1'), ('REMO2009', 'r1i1p1'), ('RCA4', 'r1i1p1')],
+    'IPSL-IPSL-CM5A-MR': [('RCA4', 'r1i1p1')],
+    'NCC-NorESM1-M': [('RCA4', 'r1i1p1')],
     #'CNRM-CERFACS-CNRM-CM5': []
 }
-vars = ['tasAdjust', 'prAdjust']
+variables = ['tasAdjust', 'prAdjust']
 
 conn = SearchConnection('https://esgf-data.dkrz.de/esg-search', distrib=True)
+# ctx = conn.new_context(bias_adjustment = 'v1-LSCE-IPSL-CDFt-EOBS10-1971-2005', **search_args)
 
-ctx = conn.new_context(driving_model = list(chains.keys()), experiment = 'rcp45', **search_dict)
-ctx.facet_counts['driving_model']
-ctx.facet_counts['bias_adjustment']
-#ctx.facet_counts['rcm_name']
+# ctx.facet_counts['driving_model']
+# ctx.facet_counts['bias_adjustment']
+# ctx.facet_counts['rcm_name']
+# ctx.facet_counts['ensemble']
 
-results = ctx.search()
-for dataset in results:
-    info = dataset.json
-    driving_model = info['driving_model'][0]
-    rcm_name = info['rcm_name'][0]
-    var = info['variable'][0]
+all_files = []
+for gcm, rcms in chains.items():
+    for rcm, ensemble in rcms:
+        for var in variables:
 
-    if rcm_name not in chains[driving_model]:
-        continue
+            ctx = conn.new_context(
+                driving_model=gcm,
+                rcm_name=rcm,
+                experiment="rcp45",
+                variable=var,
+                ensemble=ensemble,
+                #bias_adjustment = 'v1-LSCE-IPSL-CDFt-EOBS10-1971-2005',
+                **search_args
+            )
+            ds_hist = ctx.search()
 
-    files = dataset.file_context().search()
-    fnames = [i.filename for i in files]
-    urls = [i.opendap_url for i in files]
+            if len(ds_hist) > 1:
+                #raise ValueError('More than 1 dataset found. Refine search!')
+                print(f'More than 1 dataset found for {gcm}-{rcm}-{ensemble}-{var}')
+                print(ctx.facet_counts['bias_adjustment'])
+                continue
+            elif len(ds_hist) == 0:
+                print(f'No datasets found for {gcm}-{rcm}-{ensemble}-{var}')
+                continue
 
-    ctx_sim = ctx = conn.new_context(
-        driving_model=driving_model,
-        rcm_name=rcm_name,
-        variable=var,
-        experiment="rcp45",
-        **{i: j for i, j in search_dict.items() if i != "variable"}
-    )
-    results_sim = ctx_sim.search()
+            files = ds_hist[0].file_context().search()
+            fnames = [i.filename for i in files]
+            #urls = [i.opendap_url for i in files]
 
-    if len(results_sim) > 1:
-        raise ValueError('More than 1 dataset found. Refine search!')
-    sys.exit()
+            all_files.append(fnames)
+            print(f"{gcm}-{rcm}-{ensemble}-{var}")
+            continue
 
-    # xr.open_dataset(files[0].opendap_url)
+            # xr.open_dataset(files[0].opendap_url)
+
+            sys.exit()
+
+
+
+
+
 
 test = xr.open_mfdataset(urls[0:2], preprocess = lambda x: x.sel(rlat = slice(-5, 0), rlon = slice(-10, -5)))
 test.tasAdjust.isel(time = 0).plot()
