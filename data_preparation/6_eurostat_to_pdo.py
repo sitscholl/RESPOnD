e25e133ab377ec2474612847f59db8ceebedac32
sitscholl <tscholl.simon@gmail.com>
2024-11-19 15:49:45 +0100

    Initial commit: Downscale cultivation area for a selected variety to PDO level

---
+++ data_preparation/6_eurostat_to_pdo.py
@@ -0,0 +1,84 @@
+import pandas as pd
+from pathlib import Path
+import numpy as np
+import geopandas as gpd
+import urllib.request
+
+pdo_path = Path('prepared_data/EU_PDO.gpkg')
+if not pdo_path.is_file():
+    urllib.request.urlretrieve(r"https://springernature.figshare.com/ndownloader/files/35955185", pdo_path)
+
+# Test region
+xmin, ymin, xmax, ymax = 3343824,1882606,3363178,1905273
+
+pdo_shp = gpd.read_file(pdo_path)#.cx[xmin:xmax, ymin:ymax]
+vineyards_shp = gpd.read_file('../data/vineyards_europe_lau.shp')#.cx[xmin:xmax, ymin:ymax]
+pdo_name_map = pd.read_csv('prepared_data/candiago_2022.csv')
+
+eurostat = gpd.read_file('prepared_data/eurostat_2020.shp')
+eurostat['Area'] = eurostat['Area 2020'].fillna(eurostat['Area 2015'])
+eurostat_shp = eurostat[["NUTS_ID", "geometry"]].drop_duplicates()
+
+varieties = np.sort(eurostat['Prime Name'].dropna().unique())
+
+##Intersect PDOs and vineyards
+pdo_vineyards = (
+    gpd.overlay(pdo_shp, vineyards_shp, how="intersection", keep_geom_type=True)
+    .explode()
+)
+
+del vineyards_shp
+
+pdo_vineyards = (
+    pdo_vineyards.loc[pdo_vineyards.geometry.area >= 10000]
+    .copy()
+    .dissolve(by="PDOid")
+    .reset_index()
+    .drop("LAUid", axis=1)
+)
+print('Intersected vineyards')
+
+##Calculate vineyard area per NUTS
+# eurostat_vineyards = gpd.overlay(eurostat_shp, vineyards_shp, how = 'intersection', keep_geom_type=True)
+# eurostat_vineyards = (
+#     eurostat_vineyards.loc[eurostat_vineyards.geometry.area >= 100000]
+#     .dissolve(by = 'NUTS_ID')
+#     .reset_index()
+#     .drop(['LAUid'], axis = 1)
+# )
+# eurostat_vineyards['Vin_Area_NUTS'] = eurostat_vineyards.geometry.area
+# eurostat_vineyards.drop(['geometry'], axis = 1, inplace = True)
+
+var_name = "GEWUERZTRAMINER"
+
+##Select pdos and nuts that contain variety
+pdos_var = pdo_name_map.loc[pdo_name_map['Prime Name'] == var_name, 'PDOid'].unique()
+nuts_var = eurostat.loc[eurostat['Prime Name'] == var_name, 'NUTS_ID'].unique()
+
+##Intersect PDO vineyards and Eurostat
+pdo_vineyards_nuts = gpd.overlay(
+    pdo_vineyards.loc[pdo_vineyards["PDOid"].isin(pdos_var)],
+    eurostat_shp.loc[eurostat_shp["NUTS_ID"].isin(nuts_var)],
+    how="intersection",
+    keep_geom_type=True,
+)
+
+if pdo_vineyards_nuts.shape[0] > 0:
+
+    pdo_vineyards_nuts = (
+        pdo_vineyards_nuts.loc[pdo_vineyards_nuts.geometry.area >= 100000]
+        .copy()
+    )
+    pdo_vineyards_nuts['Vin_Area_PDO'] = pdo_vineyards_nuts.geometry.area
+    pdo_vineyards_nuts['Vin_Area_NUTS'] = pdo_vineyards_nuts.groupby('NUTS_ID')['Vin_Area_PDO'].transform("sum")
+    pdo_vineyards_nuts = pdo_vineyards_nuts.merge(
+        eurostat.loc[eurostat["Prime Name"] == var_name, ["NUTS_ID", "Area"]],
+        on="NUTS_ID",
+        how="left",
+    )
+
+    pdo_vineyards_nuts['Cultivation Area'] = pdo_vineyards_nuts['Area'] * (pdo_vineyards_nuts['Vin_Area_PDO'] / pdo_vineyards_nuts['Vin_Area_NUTS'])
+    
+    pdo_cult_area = pdo_vineyards_nuts.groupby('PDOid', as_index = False)['Cultivation Area'].sum()
+    pdo_cult_area['Variety'] = var_name
+    ##
