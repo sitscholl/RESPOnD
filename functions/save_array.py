43d3e5f60a5779a8efed121d29180161c2c49ba4
sitscholl <tscholl.simon@gmail.com>
2025-01-09 09:20:37 +0100

    dynamic creation of slices to allow different dimension order

--- functions/save_array.py
+++ functions/save_array.py
@@ -3,15 +3,14 @@ import xarray as xr
 from pathlib import Path
 import datetime
 import warnings
+import numpy as np
 
 ####testing
-# import numpy as np
-# 
 # lx, ly, lz = 100, 100, 2
-# data = xr.DataArray(np.random.rand(lx, ly, lz)*100, coords = {'lon': np.arange(0, lx), 'lat': np.arange(0, ly), 'time': [datetime.datetime(2024,1,4), datetime.datetime(2024,1,5)]})
+# data = xr.DataArray(np.random.rand(lx, lz, ly), coords = {'lon': np.arange(0, lx), 'time': [datetime.datetime(2024,1,2), datetime.datetime(2024,1,3)], 'lat': np.arange(0, ly)})
 # filename = 'thrash/save_array_test.nc'
 # unlimited_dim = 'time'
-# overwrite = False
+# overwrite = True
 
 # Path(filename).unlink()
 
@@ -38,7 +37,7 @@ def save_array(data, filename, unlimited_dim = None, overwrite = False):
         filename = Path(filename)
 
     if not filename.is_file():
-        data.to_netcdf(filename, mode='w', unlimited_dim = [unlimited_dim]) #, encoding = {param: dict(zlib = True, complevel = 9)}
+        data.to_netcdf(filename, mode='w', unlimited_dims = [unlimited_dim]) #, encoding = {param: dict(zlib = True, complevel = 9)}
     else:
         _append_to_netcdf(filename, data, unlimited_dim = unlimited_dim, overwrite = overwrite)
 
@@ -49,18 +48,21 @@ def _append_to_netcdf(filename, ds_to_append, unlimited_dim, overwrite = False):
 
         nc_coord = nc[unlimited_dim]
 
-        ##Transform coordinate values to netCDF units
-        dt_coords = [
-            datetime.datetime(j, m, d)
-            for d, m, j in zip(
-                ds_to_append[unlimited_dim].dt.day.values,
-                ds_to_append[unlimited_dim].dt.month.values,
-                ds_to_append[unlimited_dim].dt.year.values,
+        if ds_to_append[unlimited_dim].dtype == np.dtype('<M8[ns]'):
+            ##Transform datetime values to netCDF units
+            dt_coords = [
+                datetime.datetime(j, m, d)
+                for d, m, j in zip(
+                    ds_to_append[unlimited_dim].dt.day.values,
+                    ds_to_append[unlimited_dim].dt.month.values,
+                    ds_to_append[unlimited_dim].dt.year.values,
+                )
+            ]
+            dt_num = netCDF4.date2num(
+                dt_coords, units=nc_coord.units, calendar=nc_coord.calendar
             )
-        ]
-        dt_num = netCDF4.date2num(
-            dt_coords, units=nc_coord.units, calendar=nc_coord.calendar
-        )
+        else:
+            dt_num = list( ds_to_append[unlimited_dim].values )
 
         contained_dt = [i in nc_coord[:] for i in dt_num]
 
@@ -69,12 +71,18 @@ def _append_to_netcdf(filename, ds_to_append, unlimited_dim, overwrite = False):
                 raise ValueError(f"All slices in dimension {unlimited_dim} already contained in output file!")
             elif any(contained_dt):
                 warnings.warn(f"Some slices of dimension {unlimited_dim} already contained in output file! Only new slices are written.")
-            
+
                 ##Remove already contained coords
                 ds_to_append = ds_to_append.sel({unlimited_dim: [not i for i in contained_dt]})
                 dt_num = [dt_num[i] for i,j in enumerate(contained_dt) if not j]
 
         for var_name in ds_to_append.keys():
+
+            if not var_name in nc.variables:
+                #TODO: Support adding new variables to file
+                warnings.warn(f"Variable {var_name} not found in existing nc file. Adding new variables is not supported.")
+                continue
+
             expand_data = ds_to_append[var_name]
             nc_variable = nc[var_name]
 
@@ -95,7 +103,12 @@ def _append_to_netcdf(filename, ds_to_append, unlimited_dim, overwrite = False):
                 else:
                     nc_idx.append(len(nc_coord[:])+l)
                     l += 1
-            
-            print(nc_idx)
-            nc_variable[:,:,nc_idx] = data_encoded.data
+
+            left_slices = list(nc_variable.dimensions).index(unlimited_dim)
+            nc_slice = (
+                (slice(None),) * left_slices
+                + (nc_idx, )
+                + (slice(None),) * (len(nc.dimensions) - left_slices - 1)
+            )
+            nc_variable[nc_slice] = data_encoded.transpose(*nc_variable.dimensions).data
             nc_coord[nc_idx] = dt_num
